28db26af5d1895b1027e67346710d5d73a7d34c7
Luigi Santamaría <luissantamaria@macbook-pro-de-luis.local>
2016-07-11 11:55:34 -0500

    Initial Commit

---
+++ pruebagit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebagit
+//
+//  Created by Luis Santamaría on 11/07/16.
+//  Copyright © 2016 Santamaria Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
